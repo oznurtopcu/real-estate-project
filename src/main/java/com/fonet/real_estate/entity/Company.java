54611c17a4fc9020abbffc200138951cc6cd9f45
oznurtopcu <topcuoznur121@gmail.com>
2025-10-07 21:53:32 +0300

    create company entity

---
+++ src/main/java/com/fonet/real_estate/entity/Company.java
@@ -0,0 +1,52 @@
+package com.fonet.real_estate.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "company", schema = "real_estate")
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotEmpty
+    @Size(max = 150)
+    private String name;
+
+    private String address;
+
+    @Size(max = 30)
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Size(max = 70)
+    @Column(name = "authorized_person")
+    private String authorizedPerson;
+
+
+    @OneToMany(mappedBy = "company", cascade = CascadeType.ALL)
+    private Set<RealEstate> realEstates;
+
+    private void addRealEstate(RealEstate realEstate) {
+
+        if(realEstates == null)
+            realEstates = new HashSet<>();
+
+        realEstates.add(realEstate);
+    }
+
+}
