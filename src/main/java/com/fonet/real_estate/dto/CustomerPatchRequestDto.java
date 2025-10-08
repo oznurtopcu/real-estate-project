cf41c75d23ee4545a6e2e2b8a7a2f3afe83981c9
oznurtopcu <topcuoznur121@gmail.com>
2025-10-09 00:47:16 +0300

    create customer request dto

---
+++ src/main/java/com/fonet/real_estate/dto/CustomerPatchRequestDto.java
@@ -0,0 +1,34 @@
+package com.fonet.real_estate.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerPatchRequestDto {
+
+    @Size(max = 45)
+    @JsonProperty(value = "first_name")
+    private String firstName;
+
+    @Size(max = 45)
+    @JsonProperty(value = "last_name")
+    private String lastName;
+
+    @Size(max = 30)
+    @JsonProperty(value = "phone_number")
+    private String phoneNumber;
+
+    @Email
+    @Size(max = 100)
+    private String email;
+}
