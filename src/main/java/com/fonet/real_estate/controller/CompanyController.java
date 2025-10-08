c8bd90abe4ff4486ff026738d2ed8f7c1593ad77
oznurtopcu <topcuoznur121@gmail.com>
2025-10-08 23:40:50 +0300

    add dto to company controller

--- src/main/java/com/fonet/real_estate/controller/CompanyController.java
+++ src/main/java/com/fonet/real_estate/controller/CompanyController.java
@@ -1,5 +1,8 @@
 package com.fonet.real_estate.controller;
 
+import com.fonet.real_estate.dto.CompanyPatchRequestDto;
+import com.fonet.real_estate.dto.CompanyRequestDto;
+import com.fonet.real_estate.dto.CompanyResponseDto;
 import com.fonet.real_estate.entity.Company;
 import com.fonet.real_estate.service.CompanyService;
 import jakarta.validation.constraints.Positive;
@@ -21,33 +24,150 @@ public class CompanyController {
         this.companyService = companyService;
     }
 
+
     @GetMapping
-    public List<Company> getCompanies() {
-        return companyService.getAll();
+    public List<CompanyResponseDto> getCompanies() {
+
+        //return companyService.getAll();
+        //TODO: companyResponseDto dönüştürme işlemini farklı bir dosyaya taşı
+        return companyService.getAll()
+                .stream()
+                .map(company -> new CompanyResponseDto(
+                        company.getName(),
+                        company.getAddress(),
+                        company.getPhoneNumber(),
+                        company.getAuthorizedPerson()))
+                .toList();
     }
 
     @GetMapping("/{id}")
-    public Company getCompanyById(@Positive @PathVariable("id") Long id) {
-        return companyService.findById(id);
+    public CompanyResponseDto getCompanyById(@Positive @PathVariable("id") Long id) {
+
+        Company company = companyService.findById(id);
+
+        //TODO: companyResponseDto dönüştürme işlemini farklı bir dosyaya taşı
+        CompanyResponseDto companyResponseDto = new CompanyResponseDto(
+                company.getName(),
+                company.getAddress(),
+                company.getPhoneNumber(),
+                company.getAuthorizedPerson());
+
+        return companyResponseDto;
     }
+
+//    @PostMapping
+//    @ResponseStatus(HttpStatus.CREATED)
+//    public CompanyResponseDto create(@Validated @RequestBody Company newCompany) {
+//        //return companyService.create(company);
+//
+//
+//        Company company = companyService.create(newCompany);
+//
+//        //TODO: companyResponseDto dönüştürme işlemini farklı bir dosyaya taşı
+//        CompanyResponseDto companyResponseDto = new CompanyResponseDto(
+//                company.getName(),
+//                company.getAddress(),
+//                company.getPhoneNumber(),
+//                company.getAuthorizedPerson());
+//
+//        return companyResponseDto;
+//    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Company create(@Validated @RequestBody Company company) {
-        return companyService.create(company);
+    public CompanyResponseDto create(@Validated @RequestBody CompanyRequestDto companyRequestDto) {
+        //return companyService.create(company);
+
+        //TODO: companyRequestDto için ayrı bir fonksiyon yaz(başka bir dosyada)
+        Company company = new Company();
+        company.setName(companyRequestDto.getName());
+        company.setAddress(companyRequestDto.getAddress());
+        company.setPhoneNumber(companyRequestDto.getPhoneNumber());
+        company.setAuthorizedPerson(companyRequestDto.getAuthorizedPerson());
+
+        company = companyService.create(company);
+
+        //TODO: companyResponseDto dönüştürme işlemini farklı bir dosyaya taşı
+        CompanyResponseDto companyResponseDto = new CompanyResponseDto(
+                company.getName(),
+                company.getAddress(),
+                company.getPhoneNumber(),
+                company.getAuthorizedPerson());
+
+        return companyResponseDto;
     }
+
+//    @PutMapping("/{id}")
+//    public CompanyResponseDto replaceOrCreate(@Positive @PathVariable("id") Long id,
+//                                   @Validated @RequestBody Company company) {
+//
+//        Company updatedCompany = companyService.replaceOrCreate(id, company);
+//        CompanyResponseDto companyResponseDto = new CompanyResponseDto(
+//                updatedCompany.getName(),
+//                updatedCompany.getAddress(),
+//                updatedCompany.getPhoneNumber(),
+//                updatedCompany.getAuthorizedPerson());
+//
+//        return companyResponseDto;
+//    }
 
     @PutMapping("/{id}")
-    public Company replaceOrCreate(@Positive @PathVariable("id") Long id,
-                                   @Validated @RequestBody Company company) {
-        return companyService.replaceOrCreate(id, company);
+    public CompanyResponseDto replaceOrCreate(@Positive @PathVariable("id") Long id,
+                                              @Validated @RequestBody CompanyRequestDto companyRequestDto) {
+
+        Company company = new Company();
+        company.setName(companyRequestDto.getName());
+        company.setAddress(companyRequestDto.getAddress());
+        company.setPhoneNumber(companyRequestDto.getPhoneNumber());
+        company.setAuthorizedPerson(companyRequestDto.getAuthorizedPerson());
+
+        Company updatedCompany = companyService.replaceOrCreate(id, company);
+        CompanyResponseDto companyResponseDto = new CompanyResponseDto(
+                updatedCompany.getName(),
+                updatedCompany.getAddress(),
+                updatedCompany.getPhoneNumber(),
+                updatedCompany.getAuthorizedPerson());
+
+        return companyResponseDto;
     }
 
+
+//    @PatchMapping("/{id}")
+//    public CompanyResponseDto update(@Positive @PathVariable("id") Long id,
+//                          @Validated @RequestBody Company company) {
+//        //return companyService.update(id,company);
+//
+//        Company updatedCompany = companyService.update(id, company);
+//        CompanyResponseDto companyResponseDto = new CompanyResponseDto(
+//                updatedCompany.getName(),
+//                updatedCompany.getAddress(),
+//                updatedCompany.getPhoneNumber(),
+//                updatedCompany.getAuthorizedPerson());
+//
+//        return companyResponseDto;
+//    }
+
     @PatchMapping("/{id}")
-    public Company update(@Positive @PathVariable("id") Long id,
-                          @Validated @RequestBody Company company) {
-        return companyService.update(id,company);
+    public CompanyResponseDto update(@Positive @PathVariable("id") Long id,
+                                     @Validated @RequestBody CompanyPatchRequestDto companyPatchRequestDto) {
+        //return companyService.update(id,company);
+
+        Company company = new Company();
+        company.setName(companyPatchRequestDto.getName());
+        company.setAddress(companyPatchRequestDto.getAddress());
+        company.setPhoneNumber(companyPatchRequestDto.getPhoneNumber());
+        company.setAuthorizedPerson(companyPatchRequestDto.getAuthorizedPerson());
+
+        Company updatedCompany = companyService.update(id, company);
+        CompanyResponseDto companyResponseDto = new CompanyResponseDto(
+                updatedCompany.getName(),
+                updatedCompany.getAddress(),
+                updatedCompany.getPhoneNumber(),
+                updatedCompany.getAuthorizedPerson());
+
+        return companyResponseDto;
     }
+
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
